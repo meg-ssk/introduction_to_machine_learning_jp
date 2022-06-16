4437379109d1d326d9df11ee47823c42a967a0c9
Meg-Ssk <syota-sasaki@syota-MacBook-Pro.local>
2022-06-16 20:22:44 +0900

    update notebook of perceptron

--- notebooks/lib/perceptron/perceptron_gates.py
+++ notebooks/lib/perceptron/perceptron_gates.py
@@ -44,33 +44,109 @@ class PerceptronGates:
 class VisualizePerceptron: 
     def __init__(self):
         self.perceptron_gates = PerceptronGates()
-        self.lattice_points_x, self.lattice_points_y = np.meshgrid(np.arange(2), np.arange(2))
-    
-    def visualize_and_gate(self):
-        # make x1 (hotizontal) array
-        horizontal_min, horizontal_max, horizontal_step = -0.5, 1.75, 0.1        
-        x1 = np.arange(horizontal_min, horizontal_max, horizontal_step)
+        # make x1 (horizontal) array 
+        self.horizontal_min, self.horizontal_max, self.horizontal_step = -0.5, 1.75, 0.1 
+        self.x1 = np.arange(self.horizontal_min, self.horizontal_max, self.horizontal_step)
         
+        self.floor_line = self.x1 * 0 - 1
+        self.ceil_line = self.x1 * 0 + 2
+        
+        
+        self.lattice_points_x, self.lattice_points_y = np.meshgrid(np.arange(2), np.arange(2))
+        
+    def visualize_single_perceptron_gate(self, weights, bias):
         # calculate slope and intercept
-        slope = -(self.perceptron_gates.weights_and_gate[0] / self.perceptron_gates.weights_and_gate[0])
-        intercept = -(self.perceptron_gates.bias_and_gate / self.perceptron_gates.weights_and_gate[0])
+        slope = -(weights[0] / weights[1])
+        intercept = -(bias / weights[1])
         
         # make x2 (vertical) array
-        x2 = slope * x1 + intercept
+        x2 = slope * self.x1 + intercept
         
         # equalise the aspect ratio
         plt.axes().set_aspect('equal')
         
         # show axes
-        plt.axhline(0, linewidth=2, color="gray")
-        plt.axvline(0, linewidth=2, color="gray")
+        plt.axhline(0, linewidth=2, color="black")
+        plt.axvline(0, linewidth=2, color="black")
         
         # set horizontal/verticallimits
         plt.xlim([-0.25, 1.5])
         plt.ylim([-0.25, 1.5])
         
-        plt.plot(x1, x2)
-        plt.scatter(self.lattice_points_x, self.lattice_points_y)
+        plt.plot(self.x1, x2)
+        plt.scatter(self.lattice_points_x, self.lattice_points_y, s=100)
+        
+        if weights[1] > 0:
+            upper_color, under_color = "b", "r"
+        else:
+            upper_color, under_color = "r", "b"
+            
+        plt.fill_between(x= self.x1, y1= x2, y2 = self.ceil_line, color= upper_color, alpha= 0.2)
+        plt.fill_between(x= self.x1, y1= x2, y2 = self.floor_line, color= under_color, alpha= 0.2)
         plt.show()
         
+        return 
+    
+    def visualize_two_layer_perceptron_gate(self, weights1, weights2, bias1, bias2):
+        # to be modified
+        
+        # calculate slope and intercept
+        slope1, slope2 = -(weights1[0] / weights1[1]), -(weights2[0] / weights2[1])
+        intercept1, intercept2 = -(bias1 / weights1[1]), -(bias2 / weights2[1]) 
+        print(intercept1, intercept2)
+        
+        # make two vertical array
+        y1, y2 = slope1 * self.x1 + intercept1, slope2 * self.x1 + intercept2
+        
+        # equalise the aspect ratio
+        plt.axes().set_aspect('equal')
+        
+        # show axes
+        plt.axhline(0, linewidth=2, color="black")
+        plt.axvline(0, linewidth=2, color="black")
+        
+        # set horizontal/verticallimits
+        plt.xlim([-0.25, 1.5])
+        plt.ylim([-0.25, 1.5])
+        
+        plt.plot(self.x1, y1, "c")
+        plt.plot(self.x1, y2, "c")
+        plt.scatter(self.lattice_points_x, self.lattice_points_y, s=100)
+        
+            
+        plt.fill_between(x= self.x1, y1= y1, y2 = self.ceil_line, color= "r", alpha= 0.2)
+        plt.fill_between(x= self.x1, y1= y1, y2 = y2, color= "b", alpha= 0.2)
+        plt.fill_between(x= self.x1, y1= y2, y2 = self.floor_line, color= "r", alpha= 0.2)
+        plt.show()
+        
+        return 
+    
+    def visualize_and_gate(self):
+        weights, bias = self.perceptron_gates.weights_and_gate, self.perceptron_gates.bias_and_gate
+        
+        # visualize perceptron by linear activate function
+        self.visualize_single_perceptron_gate(weights, bias)
         return
+    
+    def visualize_or_gate(self):
+        weights, bias = self.perceptron_gates.weights_or_gate, self.perceptron_gates.bias_or_gate
+        
+        # visualize perceptron by linear activate function
+        self.visualize_single_perceptron_gate(weights, bias)
+        return
+    
+    def visualize_nand_gate(self):
+        weights, bias = -self.perceptron_gates.weights_and_gate, -self.perceptron_gates.bias_and_gate
+        
+        # visualize perceptron by linear activate function
+        self.visualize_single_perceptron_gate(weights, bias)
+        return
+    
+    def visualize_xor_gate(self):
+        weights1, bias1 = -self.perceptron_gates.weights_and_gate, -self.perceptron_gates.bias_and_gate
+        weights2, bias2 = self.perceptron_gates.weights_or_gate, self.perceptron_gates.bias_or_gate
+        
+        # visualize perceptron by linear activate function
+        self.visualize_two_layer_perceptron_gate(weights1, weights2, bias1, bias2)
+        return
+    
